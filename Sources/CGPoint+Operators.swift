7415b6b840be755f375909d8cc755def7a4a6903
Mateusz <mateusz.fidos@intive.com>
2016-09-20 11:15:44 +0200

    CGPoint+Operators + Tests rebased (#7)

---
+++ Sources/CGPoint+Operators.swift
@@ -0,0 +1,31 @@
+//
+//  CGPoint+Operators.swift
+//  ViaSwiftUtils
+//
+//  Created by Mateusz Fidos on 16.09.2016.
+//
+//
+
+import Foundation
+
+extension CGPoint {
+    /// Overloads + operator for two CGPoints addition
+    ///
+    /// - parameter lhs: as lef hand side parameter - CGPoint type
+    /// - parameter rhs: as right hand side parameter - CGPoint type
+    ///
+    /// - returns: new CGPoint calculated from adding two given points
+    static func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return self.init(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+
+    /// Overloads - operator for two CGPoints substraction
+    ///
+    /// - parameter lhs: as lef hand side parameter - CGPoint type
+    /// - parameter rhs: as right hand side parameter - CGPoint type
+    ///
+    /// - returns: new CGPoint calculated from substractiong two given points
+    static func - (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return self.init(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+    }
+}
---
+++ iOSTests/CGPoint+OperatorsTest.swift
@@ -0,0 +1,61 @@
+//
+//  CGPoint+OperatorsTest.swift
+//  ViaSwiftUtils
+//
+//  Created by Mateusz Fidos on 16.09.2016.
+//
+//
+
+import XCTest
+@testable import ViaSwiftUtils
+
+class CGPoint_OperatorsTest: XCTestCase {
+
+    func test_plusOperatorWithSuccessScenario() {
+        //Given:
+        let point1 = CGPoint(x: 1.0, y: 1.0)
+        let point2 = CGPoint(x: 1.0, y: 1.0)
+
+        //When:
+        let point3 = point1 + point2
+
+        //Then:
+        XCTAssertEqual(point3, CGPoint(x: 2.0, y: 2.0))
+    }
+
+    func test_plusOperatorWithFailureScenario() {
+        //Given:
+        let point1 = CGPoint(x: 1.0, y: 1.0)
+        let point2 = CGPoint(x: 1.0, y: 1.0)
+
+        //When:
+        let point3 = point1 + point2
+
+        //Then:
+        XCTAssertNotEqual(point3, point1)
+    }
+
+    func test_minusOperatorWithSuccessScenario() {
+        //Given:
+        let point1 = CGPoint(x: 2.0, y: 2.0)
+        let point2 = CGPoint(x: 1.0, y: 1.0)
+
+        //When:
+        let point3 = point1 - point2
+
+        //Then:
+        XCTAssertEqual(point3, CGPoint(x: 1.0, y: 1.0))
+    }
+
+    func test_minusOperatorWithFailureScenario() {
+        //Given:
+        let point1 = CGPoint(x: 2.0, y: 2.0)
+        let point2 = CGPoint(x: 1.0, y: 1.0)
+
+        //When:
+        let point3 = point1 - point2
+
+        //Then:
+        XCTAssertNotEqual(point3, point1)
+    }
+}

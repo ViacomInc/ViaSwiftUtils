f98d13ece39ba8647c6c858a723fb75a282e8a82
Konrad Feiler <konrad.feiler@vimn.com>
2016-11-18 14:01:15 +0100

    go over the documentation again

--- Sources/ForceUnwrappingOperators.swift
+++ Sources/ForceUnwrappingOperators.swift
@@ -10,9 +10,18 @@ import Foundation
 
 infix operator !!
 
-// use like this:
-// let s = "foo"
-// i = Int(s) !! "Expecting integer, got \"\(s)\""
+/// Use like this:
+///
+/// `let s = "foo"`
+///
+/// `i = Int(s) !! "Expecting integer, got \(s)"`
+///
+/// - Parameters:
+///   - wrapped: The Optional(T) value that will be unwrapped
+///   - failureText: an autoclosure that will be printed in the case of failure
+/// - Returns: The Non-Optional value of Type T
+/// - Note:
+/// [chapter 'When to Force Unwrap' from Advanced Swift](https://www.objc.io/books/advanced-swift/)
 public func !! <T>(wrapped: T?, failureText: @autoclosure () -> String) -> T {
     if let x = wrapped { return x }
     fatalError(failureText ())
@@ -20,37 +29,78 @@ public func !! <T>(wrapped: T?, failureText: @autoclosure () -> String) -> T {
 
 infix operator !?
 
-// use like this:
-// let i = Int(s) !? "Expecting integer, got \"\(s)\""
+/// Use like this:
+///
+/// `let i = Int(s) !? "Expecting integer, got \(s)"`
+///
+/// - Parameters:
+///   - wrapped: The Optional(T) value that will be unwrapped
+///   - failureText: an autoclosure that will be printed in the case of failure
+/// - Returns: The Non-Optional value of Type T
+/// - Note:
+/// [chapter 'When to Force Unwrap' from Advanced Swift](https://www.objc.io/books/advanced-swift/)
 public func !? <T: ExpressibleByIntegerLiteral>(wrapped: T?, failureText: @autoclosure () -> String) -> T {
     assert(wrapped != nil, failureText())
     return wrapped ?? 0
 }
 
-// use like this:
-// let i = Array(s) !? "Expecting array, got \"\(s)\""
+/// use like this:
+///
+/// `let i = Array(s) !? "Expecting array, got \(s)"`
+///
+/// - Parameters:
+///   - wrapped: The Optional(T) value that will be unwrapped
+///   - failureText: an autoclosure that will be printed in the case of failure
+/// - Returns: The Non-Optional value of Type T
+/// - Note:
+/// [chapter 'When to Force Unwrap' from Advanced Swift](https://www.objc.io/books/advanced-swift/)
 public func !? <T: ExpressibleByArrayLiteral>(wrapped: T?, failureText: @autoclosure () -> String) -> T {
     assert(wrapped != nil, failureText())
     return wrapped ?? []
 }
 
-// use like this:
-// let i = String(s) !? "Expecting string, got \"\(s)\""
+/// use like this:
+///
+/// `let i = String(s) !? "Expecting string, got \(s)"`
+///
+/// - Parameters:
+///   - wrapped: The Optional(T) value that will be unwrapped
+///   - failureText: an autoclosure that will be printed in the case of failure
+/// - Returns: The Non-Optional value of Type T
+/// - Note:
+/// [chapter 'When to Force Unwrap' from Advanced Swift](https://www.objc.io/books/advanced-swift/)
 public func !? <T: ExpressibleByStringLiteral>(wrapped: T?, failureText: @autoclosure () -> String) -> T {
     assert(wrapped != nil, failureText())
     return wrapped ?? ""
 }
 
-// for arbitraty types, with default value
-// Int(s) !? (5, "Expected Integer")
+/// for arbitraty types, with default value. Used in this way:
+///
+/// `Int(s) !? (5, "Expected Integer")`
+///
+/// - Parameters:
+///   - wrapped: The Optional(T) value that will be unwrapped
+///   - nilDefault: an tuple (value, text) containing a default value and an error Text
+/// - Returns: The Non-Optional value of Type T
+/// - Note:
+/// [chapter 'When to Force Unwrap' from Advanced Swift](https://www.objc.io/books/advanced-swift/)
 public func !? <T>(wrapped: T?, nilDefault: @autoclosure () -> (value: T, text: String)) -> T {
     assert(wrapped != nil, nilDefault().text)
     return wrapped ?? nilDefault().value
 }
 
-// for void methods optional chaines, like in
-// var output: String? = nil
-// output?.write("something") !? "Wasn't expecting chained nil here"
+/// for void methods optional chains, like in
+///
+/// `var output: String? = nil`
+///
+/// `output?.write("something") !? "Wasn't expecting chained nil here"`
+///
+/// - Parameters:
+///   - wrapped: The Optional(T) value that will be unwrapped
+///   - failureText: an autoclosure that will be printed in the case of failure
+/// - Returns: The Non-Optional value of Type T
+/// - Note:
+/// [chapter 'When to Force Unwrap' from Advanced Swift](https://www.objc.io/books/advanced-swift/)
 public func !? (wrapped: ()?, failureText: @autoclosure () -> String) {
     assert(wrapped != nil, failureText)
 }

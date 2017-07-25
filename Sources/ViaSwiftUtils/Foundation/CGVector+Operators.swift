243ee8adf96147f85ebade5895f757f874e03013
Konrad Feiler <konrad.feiler@vimn.com>
2017-07-25 18:22:28 +0200

    fix import order, fix watchOS target, make sure carthage build runs

--- Sources/ViaSwiftUtils/Foundation/CGVector+Operators.swift
+++ Sources/ViaSwiftUtils/Foundation/CGVector+Operators.swift
@@ -16,8 +16,8 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-import Foundation
 import CoreGraphics
+import Foundation
 
 public extension CGVector {
     /// Overloads + operator for two CGVectors addition

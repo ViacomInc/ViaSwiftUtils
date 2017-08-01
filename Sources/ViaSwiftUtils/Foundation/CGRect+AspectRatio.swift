4991a1d8e2eace7dd11826725288b0b9fd6aa76f
Konrad Feiler <konrad.feiler@vimn.com>
2017-08-01 11:57:28 +0200

    add the forgotten tvOS

--- Sources/ViaSwiftUtils/Foundation/CGRect+AspectRatio.swift
+++ Sources/ViaSwiftUtils/Foundation/CGRect+AspectRatio.swift
@@ -16,7 +16,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-#if os(OSX) || os(iOS)
+#if os(OSX) || os(iOS) || os(tvOS)
     import CoreGraphics
 #endif
 import Foundation
--- Sources/ViaSwiftUtils/Foundation/SignedInteger+Random.swift
+++ Sources/ViaSwiftUtils/Foundation/SignedInteger+Random.swift
@@ -23,7 +23,7 @@ public extension SignedInteger {
     static func arc4random_uniform(_ upperBound: Self) -> Self {
         precondition(upperBound > 0 && upperBound.toIntMax() < UInt32.max.toIntMax(),
                      "arc4random_uniform only callable up to \(UInt32.max)")
-        #if os(OSX) || os(iOS)
+        #if os(OSX) || os(iOS) || os(tvOS)
             return numericCast(Darwin.arc4random_uniform(numericCast(upperBound)))
         #elseif os(Linux) || CYGWIN
             srandom(UInt32(time(nil)))

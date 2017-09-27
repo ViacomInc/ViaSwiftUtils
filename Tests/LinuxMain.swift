cfa7eb681047486c391723500dadd35620b9f907
Konrad Feiler <K_W_F@web.de>
2017-09-27 14:56:27 +0200

    remove linked Linux testfile , from now on use shared test files

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,7 +2,6 @@ import XCTest
 @testable import ViaSwiftUtilsTests
 
 XCTMain([
-    testCase(ViaSwiftUtilsTests.allTests),
     testCase(BetterForceUnwrappingTests.allTests),
     testCase(CGPointOperatorsTest.allTests),
     testCase(CollectionShuffledTest.allTests),
